d89d9e2e583e0c9514bafc4b1b4e5e141ac6cd60
adnanortakoy <ortakoyadnan@gmail.com>
2023-08-22 16:46:16 +0300

    second

---
+++ src/test/java/com/selenium/tests/day03/Ders07_Locators.java
@@ -0,0 +1,9 @@
+package com.selenium.tests.day03;
+
+import org.openqa.selenium.WebDriver;
+
+public class Ders07_Locators {
+    public static void main(String[] args) {
+        WebDriver driver
+    }
+}
