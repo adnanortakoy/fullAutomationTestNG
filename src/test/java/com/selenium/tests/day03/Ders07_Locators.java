8680e3749a1e939456ff4583a5bd950c8b967fab
adnanortakoy <ortakoyadnan@gmail.com>
2023-08-22 16:54:02 +0300

    second

--- src/test/java/com/selenium/tests/day03/Ders07_Locators.java
+++ src/test/java/com/selenium/tests/day03/Ders07_Locators.java
@@ -1,9 +1,13 @@
 package com.selenium.tests.day03;
 
+import com.selenium.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 
 public class Ders07_Locators {
     public static void main(String[] args) {
-        WebDriver driver
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.get("https://demoqa.com/text-box%22");
+        driver.manage().window().maximize();
+
     }
 }
